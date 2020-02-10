d732df1551b89a70fc2fd6836c82cfbe55dbb733
Milton Crisostomo <20006294@galileo.edu>
2020-02-09 23:33:23 -0600

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// actividad semana 2
+var mes = 10
+switch (mes) {
+    case 12:
+        alert("Es invierno");
+        break;
+    case 1:
+        alert("Es invierno");
+        break;
+    case 2:
+        alert("Es invierno");
+        break;
+
+    case 3:
+        alert("Es primavera");
+        break;
+    case 4:
+        alert("Es primavera");
+        break;
+    case 5:
+        alert("Es primavera");
+        break;
+
+    case 6:
+        alert("Es verano");
+        break;
+    case 7:
+        alert("Es verano");
+        break;
+    case 8:
+        alert("Es verano");
+        break;
+
+    case 9:
+        alert("Es otoño");
+        break;
+    case 10:
+        alert("Es otoño");
+        break;
+    case 11:
+        alert("Es otoño");
+        break;
+
+    default:
+        alert("no existe ese mes :´(");
+        break;
+} {
+
+
+    var carné = " 20006294"
+    var nombre = " Milton Estuardo Crisóstomo Escalante "
+
+    document.write("BIENVENIDO:  " + nombre);
+
+    document.write(" Carné " + carné);
+
+}
